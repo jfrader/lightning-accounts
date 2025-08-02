78b628375db1d8f7162d04c4128381f0519c69a9
fran <jfrader@pm.me>
2025-08-02 07:16:36 -0300

    update socket

--- src/middlewares/rateLimiter.ts
+++ src/middlewares/rateLimiter.ts
@@ -1,25 +1,27 @@
 import rateLimit, { Options } from "express-rate-limit"
 import { Request } from "express"
+import config from "../config/config"
+
+const keyGenerator = (req: Request): string => {
+  const xForwardedFor = req.headers["x-forwarded-for"]
+  const xRealIp = req.headers["x-real-ip"]
+  let clientIp: string | undefined
+
+  if (Array.isArray(xForwardedFor)) {
+    clientIp = xForwardedFor[0]?.trim() || (typeof xRealIp === "string" ? xRealIp.trim() : req.ip)
+  } else if (typeof xForwardedFor === "string") {
+    clientIp =
+      xForwardedFor.split(",")[0]?.trim() || (typeof xRealIp === "string" ? xRealIp.trim() : req.ip)
+  } else {
+    clientIp = typeof xRealIp === "string" ? xRealIp.trim() : req.ip
+  }
+
+  return clientIp || "unknown"
+}
 
 const BASE_LIMITER: Partial<Options> = {
   message: { message: "Too many requests, please try again later." },
-  keyGenerator: (req: Request): string => {
-    const xForwardedFor = req.headers["x-forwarded-for"]
-    const xRealIp = req.headers["x-real-ip"]
-    let clientIp: string | undefined
-
-    if (Array.isArray(xForwardedFor)) {
-      clientIp = xForwardedFor[0]?.trim() || (typeof xRealIp === "string" ? xRealIp.trim() : req.ip)
-    } else if (typeof xForwardedFor === "string") {
-      clientIp =
-        xForwardedFor.split(",")[0]?.trim() ||
-        (typeof xRealIp === "string" ? xRealIp.trim() : req.ip)
-    } else {
-      clientIp = typeof xRealIp === "string" ? xRealIp.trim() : req.ip
-    }
-
-    return clientIp || "unknown"
-  },
+  keyGenerator,
   handler: (_req, res) => {
     res.status(429).json({ message: "Too many requests, please try again later." })
   },
@@ -33,6 +35,9 @@ export const appLimiter = rateLimit({
 })
 
 export const authLimiter = rateLimit({
+  skip: (req) => {
+    return config.trustedProxyIps.includes(keyGenerator(req))
+  },
   windowMs: 5 * 60 * 1000,
   max: 50,
   ...BASE_LIMITER,
